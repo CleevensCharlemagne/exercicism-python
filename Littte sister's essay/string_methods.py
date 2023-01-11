42a4439b44e54c3f9f72592096f9d76649ec87ea
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-11 18:10:50 -0500

    update

--- Littte sister's essay/string_methods.py
+++ Littte sister's essay/string_methods.py
@@ -1,3 +1,6 @@
+"""Functions to help edit essay homework using string manipulation."""
+
+
 def capitalize_title(title):
     """Convert the first letter of each word in the title to uppercase if needed.
 
@@ -7,4 +10,40 @@ def capitalize_title(title):
 
     output = ''
     arr = title.split(" ")
-    return title.capitalize()
+    for word in arr:
+        output += word.capitalize() + " "
+    output = output[:-1]
+
+    return output
+
+
+def check_sentence_ending(sentence):
+    """Check the ending of the sentence to verify that a period is present.
+
+    :param sentence: str - a sentence to check.
+    :return: bool - return True if punctuated correctly with period, False otherwise.
+    """
+
+    pass
+
+
+def clean_up_spacing(sentence):
+    """Verify that there isn't any whitespace at the start and end of the sentence.
+
+    :param sentence: str - a sentence to clean of leading and trailing space characters.
+    :return: str - a sentence that has been cleaned of leading and trailing space characters.
+    """
+
+    pass
+
+
+def replace_word_choice(sentence, old_word, new_word):
+    """Replace a word in the provided sentence with a new one.
+
+    :param sentence: str - a sentence to replace words in.
+    :param old_word: str - word to replace.
+    :param new_word: str - replacement word.
+    :return: str - input sentence with new words in place of old words.
+    """
+
+    pass
