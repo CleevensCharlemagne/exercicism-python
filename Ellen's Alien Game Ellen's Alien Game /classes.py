a91510fea95b501f2ab19cade66940fc70149c11
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-15 18:47:54 -0500

    update

--- Ellen's Alien Game Ellen's Alien Game /classes.py
+++ Ellen's Alien Game Ellen's Alien Game /classes.py
@@ -19,27 +19,6 @@ class Alien:
     collision_detection(other): Implementation TBD.
     """
 
-    def __init__(self, x_coordinate, y_coor"""Solution to Ellen's Alien Game exercise."""
-
-
-class Alien:
-    """Create an Alien object with location x_coordinate and y_coordinate.
-
-    Attributes
-    ----------
-    (class)total_aliens_created: int
-    x_coordinate: int - Position on the x-axis.
-    y_coordinate: int - Position on the y-axis.
-    health: int - Amount of health points.
-
-    Methods
-    -------
-    hit(): Decrement Alien health by one point.
-    is_alive(): Return a boolean for if Alien is alive (if health is > 0).
-    teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
-    collision_detection(other): Implementation TBD.
-    """
-
     def __init__(self, x_coordinate, y_coordinate):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
@@ -51,13 +30,10 @@ class Alien:
     def is_alive(self):
         return (self.health < 1)
 
+    def teleport(self, x, y):
+        self.x_coordinate = x
+        self.y_coordinate = y
 
-
-#TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
-dinate):
-        self.x_coordinate = x_coordinate
-        self.y_coordinate = y_coordinate
-        self.health = 3
 
 
 #TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
