5724054df0406a9ee6b6ed9897ce2625d9fde5e7
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-15 20:00:52 -0500

    update

--- Leap/leap.py
+++ Leap/leap.py
@@ -1,2 +1,8 @@
 def leap_year(year):
-    pass
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                retrun False
+        return True
