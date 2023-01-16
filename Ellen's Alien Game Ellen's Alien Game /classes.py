b638817085d70a4a5ce0d227028459158b8572d4
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-15 19:28:14 -0500

    update

--- Ellen's Alien Game Ellen's Alien Game /classes.py
+++ Ellen's Alien Game Ellen's Alien Game /classes.py
@@ -25,7 +25,7 @@ class Alien:
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.health = 3
-        self.total_aliens_created += 1
+        Alien.total_aliens_created += 1
 
     def hit(self):
         self.health -= 1
