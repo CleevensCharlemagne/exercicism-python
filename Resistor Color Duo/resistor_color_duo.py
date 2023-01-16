8f223aae13b6ceb514a9548fa29ce5a5ca63286c
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-15 20:09:20 -0500

    update

--- Resistor Color Duo/resistor_color_duo.py
+++ Resistor Color Duo/resistor_color_duo.py
@@ -1,10 +1,13 @@
+def color_code(color):
+    colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+    return colors.index(color)
+
+
 def value(colors):
     output = ''
 
-    def color_code(color):
-        colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
-
-    return colors.index(color)
-
     for color in colors:
+        output += str(color_code(color))
+
+    return int(output)
 
