e21885552e5df711cb0966664ce975fcf4c24b93
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-16 19:52:20 -0500

    update

--- Grains/grains.py
+++ Grains/grains.py
@@ -1,9 +1,16 @@
 def square(number):
+    chess = []
     if number >= 1 and number <= 64:
-        return number**2
+        chess.append(1)
+        i = 1
+        while i < 64:
+            chess.append(chess[i - 1] * 2)
+            i += 1
+
     else:
         raise ValueError("square must be between 1 and 64")
 
+    return chess
 
-def total():
-    pass
+
+print(square(6))
