05207f9b8b8e20d641cc8dae49d6c1c68f307c90
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-16 19:42:18 -0500

    update

--- Grains/grains.py
+++ Grains/grains.py
@@ -1,5 +1,8 @@
 def square(number):
-    pass
+    if number >= 1 and number <= 64:
+        return number**2
+    else:
+        raise ValueError("square must be between 1 and 64")
 
 
 def total():
