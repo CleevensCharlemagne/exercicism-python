c8066952fc98087d634bf7f06fa74ec80fcb44c1
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-16 20:34:43 -0500

    update

--- Hamming /hamming.py
+++ Hamming /hamming.py
@@ -1,2 +1,9 @@
 def distance(strand_a, strand_b):
-    pass
+    distance = 0
+    if len(strand_a) == len(strand_b):
+        for i in range(len(strand_a)):
+            if starnd_a[i] != starnd_b[i]:
+                distance += 1
+    return distance
+    else:
+        raise ValueError("Strands must be of equal length.")
