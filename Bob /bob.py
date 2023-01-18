50623a78880195119691ad52236a989e7bae914b
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-17 19:02:19 -0500

    update

--- Bob /bob.py
+++ Bob /bob.py
@@ -1,2 +1,11 @@
 def response(hey_bob):
-    pass
+    if hey_bob == "How are you?":
+        return 'Sure.'
+    elif hey_bob.isupper():
+        return 'Whoa, chill out!'
+    elif hey_bob[-1] == '?':
+        return 'Calm down, I know what I'm doing!'
+    elif hey_bob == "":
+        return 'Fine. Be that way!'
+    else:
+        return 'Whatever.'
