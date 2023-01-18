d5bca270aca732a5cae9b1c7d12509fc2cdd931c
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-17 19:34:46 -0500

    update

--- Bob /bob.py
+++ Bob /bob.py
@@ -1,17 +1,19 @@
 import re
-def response(hey_bob):
-    #remove mulitple spaces
-    hey_bob = re.sub(' +', ' ',hey_bob)
-    if hey_bob[-1] == ' ':
-        hey_bob = hey_bob[:-1]
 
+
+def response(hey_bob):
+    # remove mulitple spaces
     if hey_bob == "" or hey_bob.isspace():
         return 'Fine. Be that way!'
-    elif hey_bob[-1] == '?' and hey_bob.isupper() == False:
+
+    hey_bob = re.sub(' +', ' ', hey_bob)
+    if hey_bob[-1] == ' ':
+        hey_bob = hey_bob[:-1]
+    if hey_bob[-1] == '?' and hey_bob.isupper() == False:
         return 'Sure.'
+    elif hey_bob.isupper() and hey_bob[-1] == "?":
+        return "Calm down, I know what I'm doing!"
     elif hey_bob.isupper():
         return 'Whoa, chill out!'
-    elif hey_bob[-1] == '?':
-        return "Calm down, I know what I'm doing!"
     else:
         return 'Whatever.'
