6d74982d258fdfa3a7d228145cc3b372e7b1e698
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-19 09:02:00 -0500

    update

--- Anagram /anagram.py
+++ Anagram /anagram.py
@@ -1,2 +1,14 @@
 def find_anagrams(word, candidates):
-    pass
+    anagram = set()
+
+    for candidate in candidates:
+        okay = True
+
+        if candidate != word:
+            for car in candidate
+                if car not in word:
+                    okay = False
+                    break
+        anagram.add(candidate)
+
+    return list(anagram)
