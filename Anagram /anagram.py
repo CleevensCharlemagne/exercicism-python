fda9e440d5b5ad642adb4856a7f145ec7b9143f5
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-19 09:12:30 -0500

    update

--- Anagram /anagram.py
+++ Anagram /anagram.py
@@ -6,10 +6,18 @@ def find_anagrams(word, candidates):
 
         if candidate != word:
             for car in candidate:
-                if car not in word:
-                    okay = False
-                    break
+                if (car in word) and (word.count(car) == candidate.count(car)):
+                    continue
+                else:
+                        okay = False
+                        break
         if okay:
             anagram.add(candidate)
 
     return list(anagram)
+
+
+candidates = ["enlists", "google", "inlets", "banana"]
+expected = ["inlets"]
+
+print(find_anagrams("listen", candidates))
