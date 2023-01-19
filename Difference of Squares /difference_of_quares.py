3fac611081500ec39c7065f9594eb321972ca731
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-19 09:32:32 -0500

    update

--- Difference of Squares /difference_of_quares.py
+++ Difference of Squares /difference_of_quares.py
@@ -1,9 +1,10 @@
-def square_of_sum(number):
-    pass
+import math
+def square_of_sum(n):
+    return math.pow((n * (n + 1)) / 2, 2)
 
 
-def sum_of_squares(number):
-    pass
+def sum_of_squares(n):
+    return n(n+1)(2n+1)] / 6
 
 
 def difference_of_squares(number):
