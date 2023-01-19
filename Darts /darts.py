bdf5429b640fc0a96dcf84658523b9086a3ef46d
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-18 20:19:42 -0500

    update

--- Darts /darts.py
+++ Darts /darts.py
@@ -1,14 +1,14 @@
 def score(x, y):
     point = x + y
     if point < 0:
-        point *= -1
+        point *= 0
 
     if point > 10:
         return 0
-    elif point > 5:
+    elif point > 5 and point <= 10:
         return 1
-    elif point > 1:
+    elif point >= 2 and point <= 5:
         return 5
-    else:
+    elif 1 <= point < 2:
         return 10
 
