cf945a4da3f13245484b4da1c0ad4e1185f9b8c2
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-18 19:23:12 -0500

    update

--- ETL /etl.py
+++ ETL /etl.py
@@ -1,14 +1,20 @@
 def transform(legacy_data):
     tab = []
     tab2 = []
+    output = {}
     for v in legacy_data.values():
         tab.append(v)
-    print(tab)
 
     for t in tab:
-      for e in t:
-        tab2.append(e)
+        for e in t:
+            tab2.append(e)
 
     tab2.sort()
-    print(tab2)
-transform({1: ["A", "E"], 2: ["D", "G"]})
+
+    for t in tab2:
+        for k in legacy_data.keys():
+            if t in legacy_data[k]:
+                output[t.lower()] = k
+                break
+
+    return output
