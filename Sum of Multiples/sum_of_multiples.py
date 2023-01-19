479abfe7bac3e39401699ad48c2f9f28ec3bc850
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-19 08:35:17 -0500

    update

--- Sum of Multiples/sum_of_multiples.py
+++ Sum of Multiples/sum_of_multiples.py
@@ -2,15 +2,14 @@ def sum_of_multiples(limit, multiples):
     l = set()
     output = 0
     for n in multiples:
-        for i in range(limit):
-            if i > 0:
-                if i % n == 0:
-                    l.add(i)
+      if n == 0:
+          break
+      for i in range(limit):
+        if i > 0:
+          if i % n == 0:
+            l.add(i)
 
     for el in l:
-        output += el
+      output += el
 
     return output
-
-
-print(sum_of_multiples(10, [3, 5]))
