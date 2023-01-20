a1cf6b31149b494e02edc7ccaa49ddecea63a14c
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-19 21:01:22 -0500

    update

--- Perfect Numbers/perfect_numbers.py
+++ Perfect Numbers/perfect_numbers.py
@@ -1,15 +1,17 @@
 # This function computes the factor of the argument passed
 def n_factors(number):
     factors = []
-    for i in range(1, number + 1):
+    for i in range(1, number):
         if number % i == 0:
             factors.append(i)
+    print('factors:', factors)
     return factors
 
 def sum_factors(factors):
     sum = 0
     for factor in factors:
         sum += factor
+    print('sum_factors', sum)
     return sum
 
 def classify(number):
@@ -29,4 +31,6 @@ def classify(number):
             return 'deficient'
         else:
             return 'perfect'
+
+classify(6)
 
