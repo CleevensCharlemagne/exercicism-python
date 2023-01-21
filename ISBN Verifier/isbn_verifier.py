c6385097c0c0d8de9617a5fb366d5f8597af05ab
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-20 22:03:22 -0500

    update

--- ISBN Verifier/isbn_verifier.py
+++ ISBN Verifier/isbn_verifier.py
@@ -7,7 +7,7 @@ def is_valid(isbn):
         if n.isdigit():
             isbn_digits.append(int(n))
         elif n.isalpha():
-            if n == 'X':
+            if n == 'X' and (isbn_digits.index(n) == 0 or isbn_digits.index(n) == 9):
                 isbn_digits.append(10)
             else:
                 return False
