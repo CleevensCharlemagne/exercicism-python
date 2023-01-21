24839a21236dbaeafcd917584d22d9dd13cc049e
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-20 21:21:44 -0500

    update

--- Gigasecond /gigasecond.py
+++ Gigasecond /gigasecond.py
@@ -1,4 +1,6 @@
 import datetime
+
+#Given a moment, determine the moment that would be after a gigasecond has passed.
 def add(moment):
     new_date = moment + datetime.timedelta(0,10**9)
     return new_date
