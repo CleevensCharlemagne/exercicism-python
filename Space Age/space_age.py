9553e37720e4dc5878418d507c719ed350be9353
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-21 15:45:15 -0500

    update

--- Space Age/space_age.py
+++ Space Age/space_age.py
@@ -1,5 +1,17 @@
 class SpaceAge:
+    earthYears = 31557600
+    mercuryYears = 0.2408467 * earthYears
+    venusYears = 0.61519726 * earthYears
+    marsYears = 1.8808158 * earthYears
+    jupiterYears = 11.862615 * earthYears
+    saturnYears = 29.447498 * earthYears
+    uranusYears = 84.016846 * earthYears
+    neptuneYears = 164.79132 * earthYears
+
+
     def __init__(self, seconds):
         self.seconds = seconds
 
 
+    def on_earth(self):
+        return round(self.seconds / self.earthYears, 2)
