013bd4fc7b342a762e862c91e45fc7fb10fa858c
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-20 21:19:53 -0500

    update

--- Gigasecond /gigasecond.py
+++ Gigasecond /gigasecond.py
@@ -1,6 +1,4 @@
 import datetime
 def add(moment):
-    new_date = datetime.moment + datetime.timedelta(0,10**9)
-    print(new_date.year)
-
-add(datetime(2011, 4, 25, 0, 0))
+    new_date = moment + datetime.timedelta(0,10**9)
+    return new_date
