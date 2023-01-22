59372edff04371fed460910d7b7ed1205d33a94f
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-22 16:47:45 -0500

    update

--- Wordy /wordy.py
+++ Wordy /wordy.py
@@ -1,6 +1,9 @@
 def answer(question):
     words = question.split(" ")
 
+    if '?' in words[-1]:
+        words[-1] = words[-1][:-1]
+
     if words[0] == 'what' and words[1] == 'is':
         pass
     else:
