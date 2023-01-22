347f56d7a3ee6a70e1585be62b477007a55ca904
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-22 16:49:34 -0500

    update

--- Wordy /wordy.py
+++ Wordy /wordy.py
@@ -1,10 +1,14 @@
 def answer(question):
     words = question.split(" ")
+    print(words)
 
     if '?' in words[-1]:
         words[-1] = words[-1][:-1]
 
+    print(words)
     if words[0] == 'what' and words[1] == 'is':
         pass
     else:
         raise ValueError("syntax error")
+
+answer("What is 5?")
