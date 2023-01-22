c4a56332724bf9ad4cc5daeefbbcc12ede1fc896
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-21 20:40:18 -0500

    update

--- Secret Handshake/secret_handshake.py
+++ Secret Handshake/secret_handshake.py
@@ -1,4 +1,11 @@
 def commands(binary_str):
     msg = []
-    secret = ['close your eyes', 'double blink', 'wink']
+    secret = ['jump', 'close your eyes', 'double blink', 'wink']
+
+    for i in range(4, 0, -1):
+        if binary_str[i] == '1':
+            msg.append(secret[i-1])
+
+
+
 
