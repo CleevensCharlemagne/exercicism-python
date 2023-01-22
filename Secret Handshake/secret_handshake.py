df2d2c301741849158954164a25fa0ef9ab6053b
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-21 21:32:58 -0500

    update

--- Secret Handshake/secret_handshake.py
+++ Secret Handshake/secret_handshake.py
@@ -8,6 +8,7 @@ def commands(binary_str):
         if binary_str[i] == '1':
             msg.append(secret[i-1])
 
+    # Reverse the order of the list if the bynary number starts by one
     if binary_str[0] == '1':
         msg.reverse()
 
