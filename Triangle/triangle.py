bdf642e9fe265848771cf447c2c594f9b55bfaac
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-22 20:07:51 -0500

    update

--- Triangle/triangle.py
+++ Triangle/triangle.py
@@ -1,6 +1,7 @@
 def equilateral(sides):
-    pass
-
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
 
 def isosceles(sides):
     pass
@@ -8,3 +9,9 @@ def isosceles(sides):
 
 def scalene(sides):
     pass
+
+def isTriangle(a, b, c):
+    if a + b >= c or b + c >= a or a + c >= b:
+        return True
+
+    return False
