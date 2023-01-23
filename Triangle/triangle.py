554c3617e1d12dc510330a618bc21ab93e7891f4
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-22 20:13:24 -0500

    update

--- Triangle/triangle.py
+++ Triangle/triangle.py
@@ -23,7 +23,16 @@ def isosceles(sides):
 
 
 def scalene(sides):
-    pass
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+
+    if isTriangle(a, b, c):
+        if a != b and a != c and b != c:
+            return True
+
+    return False
+
 
 def isTriangle(a, b, c):
     if a + b >= c or b + c >= a or a + c >= b:
