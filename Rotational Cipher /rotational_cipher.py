9bd612ae1896fd90e7dd4646cf4d4015a5a160d2
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-23 20:26:46 -0500

    update

--- Rotational Cipher /rotational_cipher.py
+++ Rotational Cipher /rotational_cipher.py
@@ -1,7 +1,11 @@
 def rotate(text, key):
     plain = 'abcdefghijklmnopqrstuvwxyz'
     cipher = text[key:] + text[:key]
+    output = ''
 
+    for n in text:
+        output += cipher[plain.index(n)]
 
+    return output
 
-
+print()
