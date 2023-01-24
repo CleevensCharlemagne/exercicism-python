3603aea5a26854fc9627f451f8fb58fb60756bf3
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-23 21:04:03 -0500

    update

--- Binary Search /binary _search.py
+++ Binary Search /binary _search.py
@@ -1,5 +1,7 @@
 def find(search_list, value):
-    if value not in list:
+    if value not in search_list:
         raise ValueError("value not in array")
 
     return search_list.index(value)
+
+print(find([6], 6))
