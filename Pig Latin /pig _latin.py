d143b11cec1700088243bdce6a16e6c279101270
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-24 21:53:28 -0500

    update

--- Pig Latin /pig _latin.py
+++ Pig Latin /pig _latin.py
@@ -1,2 +1,8 @@
 def translate(text):
-    pass
+    vowels = 'aeiou'
+    output = ''
+
+    if text[0] in vowels or text[:3] == 'xr' or text[:3] == 'yt':
+        output = text + 'ay'
+
+    return output
