852f1c457a21deb279074c4fffc50b116e9dba87
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-25 20:55:49 -0500

    update

--- Pig Latin /pig _latin.py
+++ Pig Latin /pig _latin.py
@@ -7,7 +7,7 @@ def translate(text):
         output = text + 'ay'
         return output
 
-    # test if text starts by qy
+    # test if text starts by qu
     if text[:2] == 'qu':
         output = text[2:] + 'quay'
         return output
