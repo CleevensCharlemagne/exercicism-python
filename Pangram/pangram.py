e597ad0c32b92b1038f8ebbc7de95f022e69d8a3
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-26 20:26:02 -0500

    update

--- Pangram/pangram.py
+++ Pangram/pangram.py
@@ -1,4 +1,5 @@
 def is_pangram(sentence):
+    # List of all the letters
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
     for letter in letters:
