e5908c94337dee9e28255385a971ecf7158e0c45
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-26 20:26:20 -0500

    update

--- Pangram/pangram.py
+++ Pangram/pangram.py
@@ -2,6 +2,7 @@ def is_pangram(sentence):
     # List of all the letters
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+    # Loop through the letters
     for letter in letters:
         if letter not in sentence.lower():
             return False
