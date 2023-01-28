07c27c515169004bb206ad4187ff1b4285cd1d00
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-27 20:26:06 -0500

    update

--- Protein Translation /protein_translation.py
+++ Protein Translation /protein_translation.py
@@ -1,9 +1,15 @@
 def proteins(strand):
+    proteins = list()
     match = {'AUG':'Methionine', 'UUU':'Phenylalanine', 'UUC':'Phenylalanine',
              'UUA':'Leucine', 'UUG':'Leucine', 'UCU':'Serine', 'UCC':'Serine',
              'UCA':'Serine', 'UCG':'Serine', 'UAU':'Tyrosine', 'UAC':'Tyrosine',
              'UGU':'Cysteine', 'UGC':'Cysteine', 'UGG':'Tryptophan', 'UAA':'STOP',
              'UAG':'STOP', 'UGA':'STOP'}
 
-    for i in range(2 , len(strand), 3):
-        print(strand[i-2:i])
+    for i in range(len(strand)+1):
+        if i % 3 == 0 and i != 0:
+            proteins.append(strand[i-3:i])
+
+    print(proteins)
+
+proteins("AUGUUUUCUUAAAUG")
