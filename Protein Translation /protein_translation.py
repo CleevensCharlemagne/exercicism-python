8638f506d5529d87266d047274311162f368cb14
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-27 19:51:02 -0500

    update

--- Protein Translation /protein_translation.py
+++ Protein Translation /protein_translation.py
@@ -1,4 +1,4 @@
 def proteins(strand):
     match = {'AUG':'Methionine', 'UUU':'Phenylalanine', 'UUC':'Phenylalanine',
              'UUA':'Leucine', 'UUG':'Leucine', 'UCU':'Serine', 'UCC':'Serine',
-             'UCA':'Serine', }
+             'UCA':'Serine', 'UCG':'Serine'}
