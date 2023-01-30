a4f2b92e6ab98120b78bfab2e6d6bd56997c5f14
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-01-29 21:39:00 -0500

    update

--- Resistor Color Trio /resistor_color+trio.py
+++ Resistor Color Trio /resistor_color+trio.py
@@ -1,5 +1,5 @@
 def label(colors):
-    colorsValue = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6}
+    colorsValue = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7}
 
 Violet: 7
 
